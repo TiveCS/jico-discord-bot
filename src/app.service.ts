045b9d4d7895a3a999321273b97bdc9e9400e072
TiveCS <ahmadfathanhanif@gmail.com>
2023-08-15 22:57:21 +0700

    ensure the bot send message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,15 +3,24 @@ import {
   Injectable,
   Logger,
   NotFoundException,
+  OnModuleInit,
 } from '@nestjs/common';
 import { DiscordService } from './discord/discord.service';
 import { SendMessageDto } from './discord/dto';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
   private readonly logger = new Logger(AppService.name);
+  private readonly TEST_CHANNEL_ID = '1132154787459244163';
 
   constructor(private readonly discord: DiscordService) {}
+
+  onModuleInit() {
+    this.sendMessage({
+      channelId: this.TEST_CHANNEL_ID,
+      message: 'Bot started',
+    });
+  }
 
   async sendMessage({ channelId, message }: SendMessageDto) {
     this.logger.log(`Sending message to ${channelId}...`);
