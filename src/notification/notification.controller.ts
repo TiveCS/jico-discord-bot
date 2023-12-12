69fad0cf308b0c9bf068e45d6eee963e6ce6ce73
TiveCS <ahmadfathanhanif@gmail.com>
2023-12-12 11:20:44 +0700

    add logger

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { DiscordSendMessageDto } from '../discord/dto';
 
 @Controller('/notification')
 export class NotificationController {
+  private logger: Logger = new Logger('NotificationController');
+
   constructor(private readonly notificationService: NotificationService) {}
 
   @Post()
   async sendMessage(@Body() dto: DiscordSendMessageDto) {
+    this.logger.log(`Sending message to ${dto.webhookUrl}`);
     await this.notificationService.sendMessage(dto.webhookUrl, dto.payload);
+    this.logger.log(`Message sent to ${dto.webhookUrl}`);
   }
 }
